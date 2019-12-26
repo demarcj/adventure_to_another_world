f0749b4c3c4ea9862ab7c20dbb31fc2469179ff9
demarcj <demarcjohnson@gmail.com>
2019-12-25 23:47:28 -0500

    Added the ability to go the 'How to Play' screen

--- components/App.vue
+++ components/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    something dsafdas
+  <div class="start_screen">
+    <h1>{{ title }}</h1>
+    <button v-on:click="startGame">{{ buttonText }}</button>
+    <p>{{ firstScript }}</p>
   </div>
 </template>
+
+<style lang="stylus" scoped>
+  .show_button{
+    display: none;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      title: `An Adventure to Another World`,
+      buttonText: `Start Game`,
+      showButton: ``,
+      firstScript: ``,
+      isGame: false
+    }
+  },
+  methods: {
+    startGame() {
+      this.isGame = !this.isGame;
+      this.buttonText = `Next`;
+      this.title = this.isGame ? `How to Play` : `An Adventure to Another World`;
+    }
+  }
+}
+</script>>
